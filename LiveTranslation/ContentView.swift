be23f22f5520aacccb3f81906aa746fab6d9aff1
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-01-27 12:04:08 -0700

    Initial Commit

---
+++ LiveTranslation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveTranslation
+//
+//  Created by Jared Davidson on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveTranslation/LiveTranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTranslationApp.swift
+//  LiveTranslation
+//
+//  Created by Jared Davidson on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveTranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
